2d1c4474cccc43010bb01845757565f3231bdcb0
Albert Onyishi <albertonyishi1@gmail.com>
2024-11-21 02:04:17 +0100

    Created a new project

---
+++ app-example/index.tsx
@@ -0,0 +1,40 @@
+import { Image, StyleSheet } from "react-native";
+
+import { Text, View } from "@/components/Themed";
+import { useEffect, useState } from "react";
+import { router } from "expo-router";
+
+export default function LandingLayout() {
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/splash");
+    }, 1500);
+  }, []);
+
+  return (
+    <View style={style.wrapper}>
+      <View style={[style.splashScreenBackground]}>
+        <Text style={style.logo}>FOOD APP</Text>
+      </View>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  wrapper: {
+    backgroundColor: "red",
+    flex: 1,
+  },
+  splashScreenBackground: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    // backgroundColor: "#ffffff",
+    backgroundColor: "#0057b7",
+  },
+  logo: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,15 @@
-import { Image, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 
-import { Text, View } from "@/components/Themed";
-import { useEffect, useState } from "react";
-import { router } from "expo-router";
-
-export default function LandingLayout() {
-  useEffect(() => {
-    setTimeout(() => {
-      router.push("/splash");
-    }, 1500);
-  }, []);
-
+export default function Index() {
   return (
-    <View style={style.wrapper}>
-      <View style={[style.splashScreenBackground]}>
-        <Text style={style.logo}>FOOD APP</Text>
-      </View>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
-
-const style = StyleSheet.create({
-  wrapper: {
-    backgroundColor: "red",
-    flex: 1,
-  },
-  splashScreenBackground: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    // backgroundColor: "#ffffff",
-    backgroundColor: "#0057b7",
-  },
-  logo: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 20,
-  },
-});
